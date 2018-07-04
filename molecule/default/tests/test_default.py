3c437f2e23f6df093c473627e89b5cb67030edfb
Daniel Dewey <testdev@asgard.int>
2018-07-04 00:29:19 -0600

    Migrated from docker to Vagrant and added sysvimrc
    - Migrated from Docker to Vagrant to allow testing of docker and docker
    service
    - Added prepare to ensure python is installed for ansible to run
    - Added central supported VIMRC to be imported by users
     - Not using central vimrc while not required, is not supported
    - Install/Enable Docker service install and tests
    - Added TODOs for future tests to be written
    - Needed to add becomes for correct permissions

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,8 +13,21 @@ def test_install_vim(host):
         assert host.package("vim-enhanced").is_installed
 
 
+def test_configure_vim(host):
+    assert host.file("/etc/vim/supportedvimrc").exists
+
+
 def test_install_docker(host):
-    assert host.package("docker").is_installed
+    if host.system_info.distribution == 'debian':
+        assert host.package("docker.io").is_installed
+    if host.system_info.distribution == 'centos':
+        assert host.package("docker").is_installed
+
+
+# TODO: Enable Docker service
+def test_docker_service(host):
+    assert host.service("docker").is_running
+    assert host.service("docker").is_enabled
 
 
 def test_install_git(host):
@@ -22,6 +35,7 @@ def test_install_git(host):
 
 
 def test_install_virtualbox(host):
+    # TODO: Check repo added
     if host.system_info.distribution == 'debian':
         assert host.package("virtualbox").is_installed
     if host.system_info.distribution == 'centos':
@@ -32,9 +46,24 @@ def test_install_tmux(host):
     assert host.package("tmux").is_installed
 
 
+# TODO: Test tmux config
+# def test_config_tmux(host):
+#    assert host.file("/etc/tmux.conf").exists
+
+
 def test_install_vscode(host):
+    # TODO: Check repo added
     assert host.package("code").is_installed
 
 
+# TODO: Install VSCode Plugins
+
+# TODO: Install Sublime
 # def test_install_sublime(host):
 #     assert host.package("docker").is_installed
+
+# TODO: Sublime plugins
+
+# TODO: Install ZSH and OhMyZSH
+
+# TODO: Configure ZSH/OhMyZSH
